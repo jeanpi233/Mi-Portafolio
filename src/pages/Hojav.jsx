6759fa27f884fb7ea4adaf1f5b14f00fbf84db42
jeanpierry moreno <165798296+jeanpi233@users.noreply.github.com>
2026-05-29 14:38:18 -0500

    Actualizar Hojav.jsx

--- src/pages/Hojav.jsx
+++ src/pages/Hojav.jsx
@@ -5,11 +5,11 @@ const Hojav = () => {
   const [mostrarModal, setMostrarModal] = useState(false);
 
   const imagenesPDF = [
-    "/HOJAV/pagina 1.jpg",
-    "/HOJAV/pagina 2.jpg",
+    "/pagina 1.jpg",
+    "/pagina 2.jpg",
   ];
 
-  const rutaPDF = "/HOJAV/HDVJPMZ.pdf";
+  const rutaPDF = "/HDVJPMZ.pdf";
 
   return (
     <section className="seccion-hoja">
